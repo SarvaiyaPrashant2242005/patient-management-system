436460c73888b6c1c6175b03b159fb12f9e8a38a
aniketsol456 <aniket.solanki.9054@gmail.com>
2025-10-27 17:03:51 +0530

    adding provider for medicine and prescription is done.

--- Patient_Management_System_Doctor/patient_management_system/lib/main.dart
+++ Patient_Management_System_Doctor/patient_management_system/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:patient_management_system/app/data/providers/auth_provider.dart';
+import 'package:patient_management_system/app/data/providers/checkup_provider.dart';
 import 'package:patient_management_system/app/modules/home/views/splash_page.dart';
 import 'package:provider/provider.dart';
 import 'app/data/providers/patient_provider.dart';
 import 'app/data/providers/clinic_provider.dart';
+import 'app/data/providers/medicine_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,9 +21,11 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => AuthProvider()),
         ChangeNotifierProvider(create: (_) => ClinicProvider()),
         ChangeNotifierProvider(create: (_) => PatientProvider()),
+        ChangeNotifierProvider(create: (_) => CheckupProvider()),
+        ChangeNotifierProvider(create: (_) => MedicineProvider()),
       ],
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Med Track',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
