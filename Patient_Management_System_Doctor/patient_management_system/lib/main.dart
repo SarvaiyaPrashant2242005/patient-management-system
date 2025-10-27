e73bf3a1b0e3694ce94e55d37f4eb92ea79a34c4
aniketsol456 <aniket.solanki.9054@gmail.com>
2025-10-27 10:33:12 +0530

    adding provider in main file is done.

--- Patient_Management_System_Doctor/patient_management_system/lib/main.dart
+++ Patient_Management_System_Doctor/patient_management_system/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:patient_management_system/app/data/providers/auth_provider.dart';
 import 'package:patient_management_system/app/modules/home/views/splash_page.dart';
+import 'package:provider/provider.dart';
+import 'app/data/providers/patient_provider.dart';
+import 'app/data/providers/clinic_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +14,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ClinicProvider()),
+        ChangeNotifierProvider(create: (_) => PatientProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const SplashPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const SplashPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
